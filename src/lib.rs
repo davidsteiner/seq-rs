b72a772d25cdd3ff32ca81391c5ab77f2864e435
David Steiner <david_j_steiner@yahoo.co.nz>
2020-04-29 23:57:49 +0100

    Render debug lines if requested

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,13 @@ mod diagram;
 mod parser;
 mod rendering;
 
-pub fn parse(content: &str) -> Result<String, parser::Error> {
+pub fn parse(content: &str, show_debug_lines: bool) -> Result<String, parser::Error> {
     let diagram = parser::create_diagram(content)?;
-    Ok(rendering::render(&diagram))
+    Ok(rendering::render(&diagram, show_debug_lines))
 }
 
 #[test]
 fn example_puml() {
     let unparsed_file = std::fs::read_to_string("example.puml").expect("cannot read puml file");
-    println!("{}", parse(&unparsed_file).expect("Parsing puml failed"));
+    println!("{}", parse(&unparsed_file, false).expect("Parsing puml failed"));
 }
--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -6,14 +6,14 @@ mod renderer;
 
 use crate::diagram::{Event, SequenceDiagram};
 use crate::rendering::group::draw_group;
-use crate::rendering::layout::{calculate_grid, SizedComponent};
+use crate::rendering::layout::{calculate_grid, SizedComponent, GridSize};
 use crate::rendering::message::draw_message;
 use crate::rendering::participant::draw_participant;
 use crate::rendering::renderer::{Renderer, SVGRenderer, MEDIUM_BLUE};
 
 use nalgebra::Point2;
 
-pub fn render(diagram: &SequenceDiagram) -> String {
+pub fn render(diagram: &SequenceDiagram, show_debug_lines: bool) -> String {
     let grid_size = calculate_grid(diagram);
     let width = grid_size.width();
     let height = grid_size.height();
@@ -61,5 +61,33 @@ pub fn render(diagram: &SequenceDiagram) -> String {
             }
         }
     }
+
+    if show_debug_lines {
+        render_debug_lines(&mut renderer, &grid_size);
+    }
+
     renderer.as_string()
+}
+
+fn render_debug_lines(renderer: &mut dyn Renderer, grid: &GridSize) {
+    for col in &grid.cols {
+        renderer.render_line(
+            Point2::new(*col, 0),
+            Point2::new(*col, grid.height()),
+            1,
+            10,
+            "#fd5600",
+            None
+        );
+    }
+    for row in &grid.row_bounds {
+        renderer.render_line(
+            Point2::new(0, *row),
+            Point2::new(grid.width(), *row),
+            1,
+            10,
+            "#fd5600",
+            None
+        );
+    }
 }
